ed83ee8ef432ea662f9074dc935802c294525d60
hatimkh20 <hatimk51@gmail.com>
2023-02-04 15:37:21 +0500

    updated the color fixes

--- app/src/main/java/com/lubnamariyam/retrofitapiinjetpackcompose/ui/theme/Theme.kt
+++ app/src/main/java/com/lubnamariyam/retrofitapiinjetpackcompose/ui/theme/Theme.kt
@@ -5,26 +5,24 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = Color(0x212121),
+    primaryVariant = Color(0x3700B3),
+    secondary = Color(0x03DAC6),
+    background = Color(0x121212),
+    surface = Color(0x424242),
+    error = Color(0xB00020)
 )
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
-
-    /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
+    primary = Color(0xBB86FC),
+    primaryVariant = Color(0x3700B3),
+    secondary = Color(0x03DAC6),
+    background = Color(0xF0F0F0),
+    surface = Color(0xFFFFFF),
+    error = Color(0xB00020)
 )
 
 @Composable
--- app/src/main/java/com/lubnamariyam/retrofitapiinjetpackcompose/view/HomeScreenUi.kt
+++ app/src/main/java/com/lubnamariyam/retrofitapiinjetpackcompose/view/HomeScreenUi.kt
@@ -1,15 +1,17 @@
 package com.lubnamariyam.retrofitapiinjetpackcompose.view
 
+import androidx.compose.animation.AnimatedContentScope.SlideDirection.Companion.End
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
@@ -17,73 +19,75 @@ import androidx.compose.ui.unit.dp
 import coil.compose.rememberImagePainter
 import coil.size.Scale
 import coil.transform.CircleCropTransformation
+import com.google.android.material.color.MaterialColors
 import com.lubnamariyam.retrofitapiinjetpackcompose.model.Movie
 
 @Composable
 fun MovieItem(movie: Movie) {
-    Card(
+    Box(
         modifier = Modifier
-            .padding(8.dp, 4.dp)
+            .padding(10.dp)
             .fillMaxWidth()
-            .height(110.dp), shape = RoundedCornerShape(8.dp), elevation = 4.dp
-    ) {
-        Surface() {
-
+            .height(IntrinsicSize.Max)
+            .clip(RoundedCornerShape(20.dp))
+            .background(color = MaterialTheme.colors.surface)
+            .border(width = 1.dp, color = MaterialTheme.colors.onSurface),
+        content = {
             Row(
-                Modifier
-                    .padding(4.dp)
+                modifier = Modifier
                     .fillMaxSize()
+                    .padding(10.dp)
             ) {
-
                 Image(
                     painter = rememberImagePainter(
                         data = movie.imageUrl,
-
                         builder = {
                             scale(Scale.FILL)
                             placeholder(coil.compose.base.R.drawable.notification_action_background)
-                            transformations(CircleCropTransformation())
-
                         }
                     ),
                     contentDescription = movie.desc,
                     modifier = Modifier
                         .fillMaxHeight()
-                        .weight(0.2f)
+                        .width(80.dp)
+                        .clip(RoundedCornerShape(8.dp))
                 )
-
-
                 Column(
-                    verticalArrangement = Arrangement.Center,
                     modifier = Modifier
                         .padding(4.dp)
                         .fillMaxHeight()
-                        .weight(0.8f)
+                        .weight(1f)
+                        .width(0.dp)
                 ) {
                     Text(
                         text = movie.name,
-                        style = MaterialTheme.typography.subtitle1,
-                        fontWeight = FontWeight.Bold
-                    )
-                    Text(
-                        text = movie.category,
-                        style = MaterialTheme.typography.caption,
+                        style = MaterialTheme.typography.h5,
+                        fontWeight = FontWeight.Bold,
+                        color = MaterialTheme.colors.onSurface,
                         modifier = Modifier
-                            .background(
-                                Color.LightGray
-                            )
-                            .padding(4.dp)
+                            .padding(bottom = 8.dp)
                     )
                     Text(
                         text = movie.desc,
-                        style = MaterialTheme.typography.body1,
-                        maxLines = 2,
-                        overflow = TextOverflow.Ellipsis
+                        style = MaterialTheme.typography.body2,
+                        maxLines = 4,
+                        overflow = TextOverflow.Ellipsis,
+                        color = MaterialTheme.colors.onSurface,
+                        modifier = Modifier.padding(top = 8.dp)
                     )
-
+                    Text(
+                        text = movie.category,
+                        style = MaterialTheme.typography.button,
+                        color = MaterialTheme.colors.onSurface,
+                        overflow = TextOverflow.Clip,
+                        modifier = Modifier
+                            .background(color = MaterialTheme.colors.secondary)
+                            .align(Alignment.End)
+                            .padding(8.dp)
+                            .clip(RoundedCornerShape(4.dp))
+                    )
                 }
             }
         }
-    }
-
+    )
 }
